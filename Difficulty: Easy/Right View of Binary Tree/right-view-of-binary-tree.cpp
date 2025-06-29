bfbd447380ba5d8c1055d7e10bca20646a1c3c2f
Prabhat Narayan Payasi <142197070+PrabhatNarayanPayasi@users.noreply.github.com>
2025-06-29 06:19:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
@@ -0,0 +1,44 @@
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+// Should return  right view of tree
+class Solution {
+  public:
+    // Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root) {
+        // Your Code here
+        queue<Node*>q;
+        vector<int>ans;
+        if(!root){
+            return ans;
+        }
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            ans.push_back(q.front()->data);
+            while(n--){
+                Node* temp = q.front();
+                q.pop();
+                if(temp->right){
+                    q.push(temp->right);
+                }
+                if(temp->left){
+                    q.push(temp->left);
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
